efbd3c446c72f408eefc04e69563cbcc6ae368aa
Parth <parthp15@yahoo.com>
2024-01-03 18:33:21 -0600

    hero section's done

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
@@ -20,9 +20,9 @@ const Computers = () => {
         shadow-mapSize={1024}
       />
       <primitive 
-        object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        object = {computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01,-0.2,-0.1]}
       />
     </mesh>
@@ -30,6 +30,22 @@ const Computers = () => {
 }
 
 const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQuery = (event) => {
+      setIsMobile(event.matches);
+    }
+    mediaQuery.addEventListener('change', handleMediaQuery);
+
+    return () => {
+      mediaQuery.removeEventListener('change',handleMediaQuery)
+    }
+  },[])
+
   return (
     <Canvas 
       frameloop='demand'
@@ -43,7 +59,7 @@ const ComputerCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile}/>
       </Suspense>
 
       <Preload all />
